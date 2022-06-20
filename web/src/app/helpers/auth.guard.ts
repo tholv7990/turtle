a41f5d7a9f5f739b638414cc016f4a6b4aef1fd5
Le Tho <tho.le@workbuddy.com>
2022-06-20 22:18:46 +0700

    styling dropdown

--- web/src/app/helpers/auth.guard.ts
+++ web/src/app/helpers/auth.guard.ts
@@ -15,19 +15,19 @@ export class AuthGuard implements CanActivate {
 
         const url = this.location.path();
 
-        if (!url.toLowerCase().startsWith('/callback')) { 
-            
-        }
 
         const token = localStorage.getItem('auth_token');
 
 
         if (token && !this.jwt.isTokenExpired(token)) {
+
             return true;
         }
 
         // not logged in so redirect to login page with the return url
         this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+
+        return false;
        
     }
 }
