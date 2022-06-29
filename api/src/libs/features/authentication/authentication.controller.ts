2db85d88ecf9ec8a00801c3a28eec64309975b2c
Le Tho <tho.le@workbuddy.com>
2022-06-29 08:54:00 +0700

    update package

--- api/src/libs/features/authentication/authentication.controller.ts
+++ api/src/libs/features/authentication/authentication.controller.ts
@@ -3,7 +3,6 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthenticateLogic } from './logic';
 import { AccountReadRepository, AccountWriteRepository } from './repositories';
-import * as AWS from "aws-sdk";
 
 
 @ApiTags('authentication')
@@ -21,6 +20,12 @@ export class AuthenticationController {
         return result;
     }
 
+    @Get('info')
+    public async getInfo(): Promise<string> {
+
+       return 'info';
+    }
+
     @Post('login')
     public async login(@Body() request: AuthenticateRequest): Promise<AuthenticateResponse> {
 
@@ -34,46 +39,6 @@ export class AuthenticationController {
         const account = await this.accountWriteRep.create(request);
 
         return account;
-    }
-
-    @Post()
-    public async save(@Body() file: any): Promise<any> {
-
-
-        const s3 = new AWS.S3
-            ({
-                secretAccessKey: 'wGwhx7LKjujpWDHCYuvWjgoShOnKAWKFt+uxSMyK',
-                accessKeyId: 'AKIATUG34ZRIIHKDCOEZ',
-                region: 'ap-southeast-1'
-            });
-
-        const params =
-        {
-            Bucket: 'turtle-trading',
-            Key: file?.name,
-            Body: file,
-            ACL: "public-read",
-            ContentType: file?.type,
-            ContentDisposition: "inline",
-            CreateBucketConfiguration:
-            {
-                LocationConstraint: "ap-southeast-1"
-            }
-        };
-
-        console.log(params);
-
-        try
-        {
-            let s3Response = await s3.upload(params).promise();
-
-            console.log(s3Response);
-        }
-        catch (e)
-        {
-            console.log(e);
-        }
-
     }
 
 }
